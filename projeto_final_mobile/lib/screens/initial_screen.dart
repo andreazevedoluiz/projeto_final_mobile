2f6cf3838f3ed327b72685f7261c1058c81d8f2d
André Luiz Azevêdo <andreazevedoluiz@gmail.com>
2023-11-24 20:23:03 -0300

    ConnectionState

--- projeto_final_mobile/lib/screens/initial_screen.dart
+++ projeto_final_mobile/lib/screens/initial_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_final_mobile/components/task.dart';
+import 'package:projeto_final_mobile/data/task_dao.dart';
 import 'package:projeto_final_mobile/data/task_inherited.dart';
 import 'package:projeto_final_mobile/screens/form_screen.dart';
-
-
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -16,21 +16,60 @@ class _InitialScreenState extends State<InitialScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       // leading: Container(),
+        // leading: Container(),
         backgroundColor: Colors.blue,
-         title: const Text(
-            "Tarefas",
-            style: TextStyle(color: Colors.white),
-          ),
+        title: const Text(
+          "Tarefas",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.only(top: 8, bottom: 70,),
-        children:  TaskInherited.of(context).taskList,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 8,
+          bottom: 70,
+        ),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: ((context, snapshot) {
+              List<Task>? itens = snapshot.data;
+              switch(snapshot.connectionState){
+
+                case ConnectionState.none:
+                break;
+
+                case ConnectionState.waiting:
+                break;
+
+                case ConnectionState.active:
+                break;
+                
+                case ConnectionState.done:
+                if (snapshot.hasData && itens != null){
+                  if(itens.isNotEmpty){
+                    return ListView.builder(itemCount: itens.length, 
+                itemBuilder: (BuildContext context, index) {
+                final Task tarefa = itens[index];
+                return tarefa;
+              }); 
+                  }
+
+                }
+                break;
+
+              }
+            }),
+            ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context, MaterialPageRoute(builder: (contextNew) =>  FormScreen(taskContext: context,),),);
+            context,
+            MaterialPageRoute(
+              builder: (contextNew) => FormScreen(
+                taskContext: context,
+              ),
+            ),
+          );
         },
         child: const Icon(Icons.add),
       ),
